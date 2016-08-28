ac216165bde463ae1d69fd00c0e5d6401ca3adf4
Andrew Fomera <andrew@zerlex.net>
2016-08-28 00:29:48 -0500

    Add first/last name and generate a username for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :first_name, :last_name
+
+  # Generate a username for our user based of the first and last name
+  before_create :set_username!
+
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  private
+    def set_username!
+      self.username = "#{self.first_name}.#{self.last_name}".downcase
+    end
 end
